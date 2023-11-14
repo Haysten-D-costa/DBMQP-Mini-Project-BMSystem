db1ea29dc4772731d0a036571d63d878ca7e72e0
Riiiya28 <96122254+Riiiya28@users.noreply.github.com>
2023-11-14 13:51:45 +0530

    Payments page done

--- php/customer.php
+++ php/customer.php
@@ -1,3 +1,4 @@
+/*
 <?php
     // reading data from login page....
     $name = $_POST['name'] ;
@@ -23,3 +24,43 @@
     }
     mysqli_close($conn);
 ?>
+
+*/
+
+
+<?php
+
+session_start();
+
+if(isset($_SESSION['name'])) {
+    echo "Welcome " . $_SESSION['name'];
+    echo "<br><a href='logout.php'>Logout</a>";
+} else {
+    header("Location: login.php");
+}
+
+    // reading data from login page....
+    $name = $_POST['name'] ;
+    $continfo = $_POST['continfo'];
+    $email = $_POST['email'];
+
+    $conn = mysqli_connect("localhost", "root", "", "BMSystem");
+    if($conn) { echo "<br>Connection successful !"; }
+    else {
+        echo "<br>Connection failed !";
+        exit();
+    }
+    $query = "
+        INSERT INTO Customers (Name, ContactInformation, Email)
+        VALUES
+        ('$name', '$continfo', '$email');
+    ";
+    $r = mysqli_query($conn, $query);
+    if($r) { echo "<br> Registration successfully !"; }
+    else {
+        echo "<br>Registration failed !";
+        exit();
+    }
+    mysqli_close($conn);
+?>
+
---
+++ php/payment.php
@@ -0,0 +1,85 @@
+<?php
+
+    // Retrieve form data
+    // $paymentID = $_POST["paymentID"];
+    $paymentDate = $_POST["paymentDate"];
+	$paymentType = $_POST["paymentType"];
+    $amount = $_POST["amount"];
+
+    // Display payment details
+    echo "<h2>Payment Details</h2>";
+    
+    // echo "<br><br><b>Payment ID: </b>" .$paymentID;
+    echo "<br><br><b>Payment Date: </b>" .$paymentDate;
+    echo "<br><b>Payment Type: </b>" .$paymentType;
+    echo "<br><b>Amount: </b>" .$amount;
+	
+	
+/*	
+    $paymentID = $_POST['paymentID'];
+    $paymentDate = $_POST['paymentDate'];
+	$paymentType = $_POST['paymentType'] ;
+	$amount = $_POST['amount'];
+
+    echo "<h2>Payment Details</h2>";
+    echo "<br><b>Payment Type: </b>" .$paymentType;
+    echo "<br><br><b>Payment ID: </b>" .$paymentID;
+    echo "<br><br><b>Payment Date: </b>" .$paymentDate;
+    echo "<br><br><b>Amount: </b>" .$amount;
+*/	
+
+
+    $conn = mysqli_connect("localhost", "root", "", "BMSystem");
+    if($conn) { echo "<br>Connection successful !"; }
+    else {
+        alert("<br>Connection failed !");
+        exit();
+    }
+	
+    $query = "
+        INSERT INTO Payments (PaymentDate, PaymentType, Amount)
+        VALUES
+        ('$paymentDate', '$paymentType', '$amount');
+    ";
+    $r = mysqli_query($conn, $query);
+    if($r) { echo "<br> Payment Successfull !"; }
+    else {
+        echo "<br> Payment failed !";
+        exit();
+    }
+    mysqli_close($conn);
+
+
+/*
+        // Connect to the database
+        $conn = mysqli_connect("localhost", "root", "", "BMSystem");
+
+        if($conn) {
+            echo "<br>Connection successful !";
+        } else {
+            echo "<br>Connection failed !";
+            exit();
+        }
+    
+        // Insert data into Payments table
+        $query = "
+            INSERT INTO Payments (PaymentID, PaymentDate, PaymentType, Amount)
+            VALUES
+            ('$paymentID', '$paymentDate', '$paymentType', '$amount');
+        ";
+    
+        $r = mysqli_query($conn, $query);
+    
+        if($r) {
+            echo "<br>Payment Successful !";
+        } else {
+            echo "<br>Payment failed !";
+            exit();
+        }
+    
+        // Close the connection
+        mysqli_close($conn);
+
+*/
+
+?>
