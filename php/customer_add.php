d63048f66b424e1d589a7619c7c200040c2f4861
Sehal Chodankar <sehalchodankar11@gmail.com>
2023-11-21 11:00:48 +0530

    customer page update

---
+++ php/customer_add.php
@@ -0,0 +1,28 @@
+<?php
+	$name = $_POST['name'] ;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $conn = mysqli_connect("localhost", "root", "", "BMSystem");
+    if($conn) { echo "<br>Connection successful !"; }
+    else {
+        alert("<br>Connection failed !");
+        exit();
+    }
+    $query = "
+        INSERT INTO LoginDetails (userName, email, password)
+        VALUES
+        ('$name', '$email', '$password');
+    ";
+    $r = mysqli_query($conn, $query);
+    if($r) { 
+        echo "<br> Recored inserted successfully !";
+    }
+    else {
+        echo "<br>Record insertion failed !";
+        exit();
+    }
+    mysqli_close($conn);
+
+
+?>
